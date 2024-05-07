8ceec3db68bb92aafc596d033dac7fc0600d511f
njambi-muhoro <muhoronjambi@gmail.com>
2024-05-06 20:09:29 -0400

    Covering list and tuples

---
+++ Lists_and_Tuplets.py
@@ -0,0 +1,12 @@
+fruits = ["mango", "banana","apple"]
+print(fruits[1])
+
+repeated_tuple = fruits*3
+print(repeated_tuple)
+
+veg = ['tomato','onions']
+joined_veg_and_fruit = veg + fruits
+print(joined_veg_and_fruit)
+
+sliced_tuple = (fruits[1:])
+print(sliced_tuple)
